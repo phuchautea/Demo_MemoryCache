2c16182a095576a64ae6bc63f3b18517643a5ca2
phuchau.tea <phuchau.tea@gmail.com>
2023-03-20 08:58:32 +0700

    Add project files.

---
+++ Demo_MemoryCache/Controllers/SachController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Runtime.Caching;
+using Demo_MemoryCache.Models;
+
+namespace Demo_MemoryCache.Controllers
+{
+    public class SachController : Controller
+    {
+        private static MemoryCache _bookCache = new MemoryCache("bookCache");
+        MyDataDataContext data = new MyDataDataContext();
+        public List<Sach> GetAll()
+        {
+            var books = _bookCache.Get("bookCache") as List<Sach>;
+            if (books == null)
+            {
+                books = (from tt in data.Saches select tt).ToList();
+                _bookCache.Add("bookCache", books, DateTimeOffset.Now.AddSeconds(30));
+            }
+            return books;
+        }
+        public ActionResult Index()
+        {
+            var allBooks = GetAll();
+            return View(allBooks);
+        }
+    }
+}
